22432a42544b892a5c79933c93e0c522c4815b69
KantoWatanabe <>
2025-04-13 13:41:17 +0900

    GitHub Pages公開のためpublic->docsに変更

--- src/utils/writer.ts
+++ src/utils/writer.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function saveJson(filename: string, data: unknown) {
-  const filepath = path.resolve(process.cwd(), 'public', filename);
+  const filepath = path.resolve(process.cwd(), 'docs', filename);
   fs.writeFileSync(filepath, JSON.stringify(data));
   console.log(`✅ Saved ${filepath}`);
 }
